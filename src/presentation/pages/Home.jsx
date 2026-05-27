57f064406ab9fb80be3c514772fedf512c7dee4f
fabian <sajama230@gmail.com>
2026-05-26 21:10:11 -0300

    update image

--- src/presentation/pages/Home.jsx
+++ src/presentation/pages/Home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Image from "react-bootstrap/Image";
 import NavBar from "../components/NavBar";
+import img from "../../assets/hps.jpg";
 
 const Home = () => {
   return (
     <>
-      <Image
-        src="src/assets/hps.jpg"
-        width={800}
-        thumbnail
-        className="d-block mx-auto"
-      />
+      <Image src={img} width={800} thumbnail className="d-block mx-auto" />
     </>
   );
 };
